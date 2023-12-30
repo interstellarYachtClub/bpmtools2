1c7c8d4d43997a691c2765e0be7d9692ca2c19fc
Dave <interstellaryachtclub@gmail.com>
2023-12-30 13:57:25 -0500

    netlify dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte()]
+  integrations: [mdx(), sitemap(), svelte()],
+  output: "server",
+  adapter: netlify()
 });
