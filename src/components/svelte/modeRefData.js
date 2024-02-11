84153631a46edad3ca2fa071653ad602523268d9
Dave <interstellaryachtclub@gmail.com>
2024-02-11 11:24:34 -0500

    const array data abstraction / blog style

---
+++ src/components/svelte/modeRefData.js
@@ -0,0 +1,224 @@
+export const CHORDTYPE = [
+    {
+        name: 'major',
+        chordsteps: [0, 4, 7]
+    },
+    {
+        name: 'minor',
+        chordsteps: [0, 3, 7]
+    },
+    {
+        name: 'diminished',
+        chordsteps: [0, 3, 6]
+    }
+];
+
+export const MODES = [
+    {
+        name: 'aeolian',
+        label: 'Aeolian mode (minor)',
+        steps: [1, 3, 4, 6, 8, 9, 11],
+        notation: ['i', 'dim', 'III', 'iv', 'v', 'VI', 'VII']
+    },
+    {
+        name: 'locrian',
+        label: 'Locrian mode',
+        steps: [1, 2, 4, 6, 7, 9, 11],
+        notation: ['dim', 'II', 'iii', 'iv', 'V', 'VI', 'vii']
+    },
+    {
+        name: 'ionian',
+        label: 'Ionian mode (major)',
+        steps: [1, 3, 5, 6, 8, 10, 12],
+        notation: ['I', 'ii', 'iii', 'IV', 'V', 'vi', 'dim']
+    },
+    {
+        name: 'dorian',
+        label: 'Dorian',
+        steps: [1, 3, 4, 6, 8, 10, 11],
+        notation: ['i', 'ii', 'III', 'IV', 'v', 'dim', 'VII']
+    },
+    {
+        name: 'phrygian',
+        label: 'Phrygian',
+        steps: [1, 2, 4, 6, 8, 9, 11],
+        notation: ['i', 'II', 'III', 'iv', 'dim', 'VI', 'vii']
+    },
+    {
+        name: 'lydian',
+        label: 'Lydian',
+        steps: [1, 3, 5, 7, 8, 10, 12],
+        notation: ['I', 'II', 'iii', 'dim', 'V', 'vi', 'vii']
+    },
+    {
+        name: 'mixolydian',
+        label: 'Mixolydian',
+        steps: [1, 3, 5, 6, 8, 10, 11],
+        notation: ['I', 'ii', 'dim', 'IV', 'v', 'vi', 'vii']
+    }
+];
+
+export const ROOTNOTES = [
+    {
+        name: 'c',
+        label: 'C',
+        value: 0
+    },
+    {
+        name: 'c#',
+        label: 'C# / Db',
+        value: 1
+    },
+    {
+        name: 'd',
+        label: 'D',
+        value: 2
+    },
+    {
+        name: 'd#',
+        label: 'D# / Eb',
+        value: 3
+    },
+    {
+        name: 'e',
+        label: 'E',
+        value: 4
+    },
+    {
+        name: 'f',
+        label: 'F',
+        value: 5
+    },
+    {
+        name: 'f#',
+        label: 'F# / Gb',
+        value: 6
+    },
+    {
+        name: 'g',
+        label: 'G',
+        value: 7
+    },
+    {
+        name: 'g#',
+        label: 'G# / Ab',
+        value: 8
+    },
+    {
+        name: 'a',
+        label: 'A',
+        value: 9
+    },
+    {
+        name: 'a#',
+        label: 'A# / Bb',
+        value: 10
+    },
+    {
+        name: 'b',
+        label: 'B',
+        value: 11
+    }
+];
+export const TWOOCTAVES = [
+    {
+        label: 'C',
+        value: 0
+    },
+    {
+        label: 'C#/Db',
+        value: 1
+    },
+    {
+        label: 'D',
+        value: 2
+    },
+    {
+        label: 'D#/Eb',
+        value: 3
+    },
+    {
+        label: 'E',
+        value: 4
+    },
+    {
+        label: 'F',
+        value: 5
+    },
+    {
+        label: 'F#/Gb',
+        value: 6
+    },
+    {
+        label: 'G',
+        value: 7
+    },
+    {
+        label: 'G#/Ab',
+        value: 8
+    },
+    {
+        label: 'A',
+        value: 9
+    },
+    {
+        label: 'A#/Bb',
+        value: 10
+    },
+    {
+        label: 'B',
+        value: 11
+    },
+    {
+        label: 'C',
+        value: 12
+    },
+    {
+        label: 'C#/Db',
+        value: 13
+    },
+    {
+        label: 'D',
+        value: 14
+    },
+    {
+        label: 'D#/Eb',
+        value: 15
+    },
+    {
+        label: 'E',
+        value: 16
+    },
+    {
+        label: 'F',
+        value: 17
+    },
+    {
+        label: 'F#/Gb',
+        value: 18
+    },
+    {
+        label: 'G',
+        value: 19
+    },
+    {
+        label: 'G#/Ab',
+        value: 20
+    },
+    {
+        label: 'A',
+        value: 21
+    },
+    {
+        label: 'A#/Bb',
+        value: 22
+    },
+    {
+        label: 'B',
+        value: 23
+    },
+    {
+        label: 'C',
+        value: 24
+    }
+];
