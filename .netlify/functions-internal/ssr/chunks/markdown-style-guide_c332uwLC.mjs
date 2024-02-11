1f4580c059bf02ffb1258380b23a02f6ee8bea00
Dave <interstellaryachtclub@gmail.com>
2024-02-11 12:28:12 -0500

    jk run dev

--- .netlify/functions-internal/ssr/chunks/markdown-style-guide_c332uwLC.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./markdown-style-guide_KOzuclpx.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
