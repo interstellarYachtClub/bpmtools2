1d7e4e2570c7d673d3b2ce3334795b1558122300
Dave <interstellaryachtclub@gmail.com>
2024-03-08 21:08:48 -0500

    webstyle test

--- src/components/svelte/store.js
+++ src/components/svelte/store.js
@@ -3,7 +3,7 @@ import { writable } from 'svelte/store';
 // Create a writable store to hold the input value
 export const bpmGlobal = writable(120); // Initialize with a number
 export const useSamples = writable(false); //use ms by default
-export const themeSelect = writable('ableish'); //default theme
+export const themeSelect = writable('web'); //default theme
 export const modeGlobal = writable({
     name: 'aeolian',
     label: 'Aeolian mode (minor)',
